b089480bdb8126b52801f2f17964bfadfd832aa2
luis <luisgonzalez18997@gmail.com>
2019-05-04 21:55:55 -0400

    third class of var int fundamentals javascript

---
+++ primeros_pasos/clase_3/variableNum.js
@@ -0,0 +1,29 @@
+var edad = 21
+// edad = edad + 1
+edad += 1
+
+var peso = 75
+// peso = peso - 2
+peso -= 2
+
+var sandwich = 1
+
+peso += sandwich
+
+var jugarAlFutbol = 3
+
+peso -= jugarAlFutbol
+
+var precioVino = 200.3
+
+var total = Math.round(precioVino  * 100 * 3) / 100
+
+var totalStr = total.toFixed(3)
+
+var total2 = parseFloat(totalStr);
+
+
+var pizza  = 8
+var persona = 2
+
+var cantidadPorcionesPersona = pizza / persona
