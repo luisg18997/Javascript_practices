e06a99c36721c182e46894a2c3b257733cdd40e9
luis <luisgonzalez18997@gmail.com>
2019-05-05 01:31:35 -0400

    fourth class functions

---
+++ primeros_pasos/clase_4/funciones.js
@@ -0,0 +1,9 @@
+ var nombre = 'luis', edad = 21;
+
+ function imprimirEdad(n, e) {
+   console.log(`${n} tiene ${e} años`);
+ }
+
+ imprimirEdad(nombre, edad);
+ imprimirEdad('vicky', 25);
+ imprimirEdad('erick', 30);
