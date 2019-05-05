7a72aafba371afdc068417ada3c4bec891b71b68
luis <luisgonzalez18997@gmail.com>
2019-05-04 21:16:09 -0400

    first class of javascript fundamentals - first steps

---
+++ primeros_pasos/clase_1/variables.js
@@ -0,0 +1,11 @@
+console.log('hello world js');
+
+var nombre = 'luis', apellido='gonzalez';
+var edad = '21'
+
+edad = '21 años'
+
+console.log('hola ' + nombre+ ' ' + apellido);
+console.log('Tengo ' + edad);
+
+var peso = 65;
