859924778d7ad23d5c82de0c727ff6fc069a01e4
luis <luisgonzalez18997@gmail.com>
2019-05-04 21:28:55 -0400

    second class of var strings

---
+++ primeros_pasos/clase_2/variableString.js
@@ -0,0 +1,13 @@
+var nombre = 'luis', apellido='Gonzalez';
+
+var nombreEnMayusculas = nombre.toUpperCase();
+
+var apellidoEnMinusculas = apellido.toLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0)
+
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido}`;
+
+var str = nombre.substr(1, 2)
